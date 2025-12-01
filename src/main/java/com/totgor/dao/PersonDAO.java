bf00fc9b391b395dfee1eb95b23f46f6f7f18754
user <totgor@mail.ru>
2025-12-01 14:57:18 +0300

    Установил локлаьно PostgreSQL 17. Соаздал БД first_db. Создал таблицу person c полями id, name, age, email. Внес несколько человек. В pom.xml добавил зависимость org.postgrsql для работы с БД. Изменил немного класс PersonDAO для работы с БД. Создал константные строки для подключения к БД, логин и пароль. Получил класс org.postgresql.Driver для теста драйвера работы с БД. Для отображения всех людей. Созадал connection для подключения к БД. Создал statement через connection для выполнения запросов. Выполнил запрос для получения всех пользователей и загнал в ResultSet. Итеративно прохожу по всем записям в resultSet, создаю объект Person присваиваю значения этому объекту и добавляю в коллекцию people. Для сохранения и создания нового человека в БД. Так же создаю statement посредствам  connection. Выполняю запрос по вставе пользователя в БД. Вручную получая поля из объекта person, переданного через представление new.html

--- src/main/java/com/totgor/dao/PersonDAO.java
+++ src/main/java/com/totgor/dao/PersonDAO.java
@@ -1,5 +1,10 @@
 package com.totgor.dao;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,38 +16,83 @@ import com.totgor.models.Person;
 public class PersonDAO {
     
     private static int PEOPLE_COUNT = 0;
-    private List<Person> people;
+
+    private static final String URL = "jdbc:postgresql://localhost:5433/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection;
 
     {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom", 21, "tom@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob", 43, "bob@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy", 29, "katy@mail.ru"));
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            
+            while(resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        // return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        // person.setId(++PEOPLE_COUNT);
+        // people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" +  person.getName() + "'," + person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeQuery(SQL);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 
     public void update(int id, Person updatePerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
+        // Person personToBeUpdated = show(id);
+        // personToBeUpdated.setName(updatePerson.getName());
+        // personToBeUpdated.setAge(updatePerson.getAge());
+        // personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+        // people.removeIf(p -> p.getId() == id);
     }
 
 }
